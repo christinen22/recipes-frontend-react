2f105de7e44f864ad61aecdeb2ef9c833beab7e1
christinen22 <christine.elin.louise@gmail.com>
2023-07-11 20:23:49 +0200

    Working on UX and design

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./Homepage";
 import Recipes from "./Recipes";
 import SingleRecipe from "./SingleRecipe";
 import CategoryRecipes from "./CategoryRecipes";
 import Form from "../components/Form";
+import CategorySection from "../components/CategorySection";
 
 const Pages = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<CategorySection />} />
       <Route path="/recipes" element={<Recipes />} />
       <Route path="/recipes/:id" element={<SingleRecipe />} />
       <Route path="/categories/:id" element={<CategoryRecipes />} />
