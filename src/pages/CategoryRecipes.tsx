705d41ec30055dc2db35a92258c244bcadfa9dfb
christinen22 <christine.elin.louise@gmail.com>
2023-07-21 21:35:15 +0200

    WIP UX and fixed som Category-stuff

--- src/pages/CategoryRecipes.tsx
+++ src/pages/CategoryRecipes.tsx
@@ -5,6 +5,7 @@ import { IRecipe } from "../types";
 import { useNavigate, NavLink } from "react-router-dom";
 import { Col, Container, Row, Card, Button } from "react-bootstrap";
 import Header from "../components/Header";
+//import Pagination from "../components/Pagination";
 
 interface RecipeDetails {
   id: string | undefined;
@@ -16,6 +17,8 @@ const CategoryRecipes: React.FC = () => {
   const [recipes, setRecipes] = useState<IRecipe[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  //const [totalPages, setTotalPages] = useState<number>(0);
+  //const [currentPage, setCurrentPage] = useState<number>(1);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,6 +31,7 @@ const CategoryRecipes: React.FC = () => {
 
         console.log(res.recipes);
         setRecipes(res.recipes);
+        //setTotalPages(Math.ceil(res.total / res.per_page));
       } catch (error) {
         setError("Failed to fetch recipes");
       }
@@ -51,7 +55,7 @@ const CategoryRecipes: React.FC = () => {
       {/* <Search onSearch={handleSearch} /> */}
       <Header />
       <div className="recipes">
-        <Button type="button" className="btn" onClick={() => navigate(-1)}>
+        <Button className="back-btn" onClick={() => navigate(-1)}>
           Tillbaka
         </Button>
         <Container fluid>
@@ -68,7 +72,7 @@ const CategoryRecipes: React.FC = () => {
                 <Card>
                   <Card.Img
                     variant="top"
-                    src={`https://christinensapi.com/${recipe.image}`}
+                    src={`https://christinensapi.com/${recipe?.image}`}
                     alt={recipe.title}
                   />
                   <Card.Body>
@@ -85,6 +89,14 @@ const CategoryRecipes: React.FC = () => {
             ))}
           </Row>
         </Container>
+        {/*  <div className="page-info">
+          Page {currentPage} of {totalPages}
+        </div>
+         <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />  */}
       </div>
     </>
   );
